1c422f19df9a46442d6173fbb9ad6d317e17a863
JasonLewis12 <Lewisjason64@yahoo.com>
2024-09-25 18:13:32 -0400

    feat: added logic to form and set up API to the form

--- react-form/src/components/SignUpForm.jsx
+++ react-form/src/components/SignUpForm.jsx
@@ -2,5 +2,58 @@ import React from "react";
 import { useState } from "react";
 
 export default function signUpForm() {
-  return <h2>Sign Up!</h2>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const repoonse = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      const data = await repoonse.json();
+      console.log(data);
+    } catch (error) {
+      console.error("there was in error in your POST", error);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Your username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password">Your password</label>
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
